18bdf163f9d14b6803e22ba7e7ac1e0b876d3294
Joanne Affolter <joanne.affolter@epfl.ch>
2022-11-19 00:09:49 +0100

    Ok

---
+++ src/navigation/horizontal/index.js
@@ -0,0 +1,14 @@
+import { mdiFileOutline, mdiHomeOutline } from '@mdi/js'
+
+export default [
+  {
+    title: 'Home',
+    icon: mdiHomeOutline,
+    to: 'home',
+  },
+  {
+    title: 'Second Page',
+    icon: mdiFileOutline,
+    to: 'second-page',
+  },
+]
---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: 'home',
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import('@/views/Home.vue'),
+    meta: {
+      layout: 'content',
+    },
+  },
+  {
+    path: '/second-page',
+    name: 'second-page',
+    component: () => import('@/views/SecondPage.vue'),
+    meta: {
+      layout: 'content',
+    },
+  },
+  {
+    path: '/login',
+    name: 'auth-login',
+    component: () => import('@/views/Login.vue'),
+    meta: {
+      layout: 'blank',
+    },
+  },
+  {
+    path: '*',
+    redirect: 'error-404',
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+  scrollBehavior() {
+    return { x: 0, y: 0 }
+  },
+})
+
+export default router
