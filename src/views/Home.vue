141c548b4f4abb826af0d2172ad4d5b03a55ccf0
Joanne Affolter <joanne.affolter@epfl.ch>
2022-11-19 00:56:44 +0100

    change url

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,14 +4,14 @@
       <v-card-title>Beer reviews ⚽🍺🏈</v-card-title>
       <v-card-text>Our idea is to analyze the relation between major sporting events (football world-cups, superbowl, etc.) and beer ratings to evaluate the impact of these huge events on beer consumers and breweries' sales. <br>
 More specifically we will search if those sporting events affect beers or breweries' popularity, beer consumption and consumers opinion.
-
-First of all, we will look at the most rated beers and most popular breweries to determine if there exists any temporal correlation between some major sporting events and the popularity of the studied beers/breweries.
-*For instance, can we observe a peak every 4 years in the consumption of beers from a certain brewery?*
-
-Then, we will analyze the ratings and reviews in a range of one month around the events which have caught our attention in the first part of the project. Our goal is to understand the variation in ratings and reviews between winning and losing countries.<br>
+<br>
+▪️ First of all, we will look at the most rated beers and most popular breweries to determine if there exists any temporal correlation between some major sporting events and the popularity of the studied beers/breweries.
+For instance, can we observe a peak every 4 years in the consumption of beers from a certain brewery?
+<br>
+▪️ Then, we will analyze the ratings and reviews in a range of one month around the events which have caught our attention in the first part of the project. Our goal is to understand the variation in ratings and reviews between winning and losing countries.<br>
 We will analyze the trends depending on the team's results. We will also look for the effect of these events in the textual reviews by leveraging sentiment analysis.
-
-Finally, the whole process will enable us to evaluate how to adapt the sales of breweries depending on the results.
+<br>
+▪️ Finally, the whole process will enable us to evaluate how to adapt the sales of breweries depending on the results.
 </v-card-text>
 
     </v-card>
