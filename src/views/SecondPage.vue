9bddd1098d2aff17ca68b671bb9aa3d0fb10e095
Joanne Affolter <joanne.affolter@epfl.ch>
2022-11-19 01:09:42 +0100

    change url

--- src/views/SecondPage.vue
+++ src/views/SecondPage.vue
@@ -6,7 +6,7 @@
 <span color='primary'>Aim:</span> Determine which breweries are the most suitable to be sponsors of major sporting events.<br>
 During this part, we pre-process the data to extract the features needed for the project.<br><br>
 We first have explored the matched dataset and the other two datasets to determine which one to work on.
-Our analyze has shown that the matched dataset did not contain enough data for us to work on it. We will therefore work on the datasets from each website individually.<br>
+Our analyze has shown that the matched dataset did not contain enough data for us to work on it. We will therefore work on the datasets from each website individually.<br><br>
 Then we managed to read the ratings file from both websites (RateBeer and BeerAdvocate) and extract the ratings date.
 We merged these dates to the beers dataset from each website. We also added the location of each brewery.<br>
 One important part of our pre-processing was to filter the data in order to work only with the most popular beers (i.e. with the highest number of ratings) and therefore with the breweries who could be sponsors of such events.
@@ -32,14 +32,14 @@ We started to analyze the evolution of the top 10 beers and breweries to get a g
       <span color='primary'>Aim:</span>Study whether textual reviews can be used to measure beer popularity<br><br>
 Our goal for this part is to analyze the textual reviews and see if we can derive insights from them. In particular, we aim at predicting whether a given review has a positive or negative sentiment. We think that by considering the sentiment associated to a review, we can study the popularity of certain beers better than just by looking at the numerical ratings. The intuition behind this is that a textual review is a richer source of information than a rating between 0 and 5. We hope that by leveraging sentiment analysis, we can encapsulate this additional information and use it as an additional measure for beer popularity. We therefore present a first implementation of a pipeline that:
 
-
-        - Extracts textual reviews from the dataset
-        - Annotates the reviews (positive/negative for later training) using the ratings
-        - Represents each review in an embedding space
-        - Trains a classifier on the review embeddings
-        - Uses the classifier to predict the sentiment of a given review
-        - Provides a ranking of beer popularity based on sentiment analysis
-
+<v_list>
+    <v-list-item>- Extracts textual reviews from the dataset</v-list-item>
+    <v-list-item>- Annotates the reviews (positive/negative for later training) using the ratings</v-list-item>
+    <v-list-item>- Represents each review in an embedding space</v-list-item>
+    <v-list-item>- Trains a classifier on the review embeddings</v-list-item>
+    <v-list-item> - Uses the classifier to predict the sentiment of a given review</v-list-item>
+    <v-list-item> - Provides a ranking of beer popularity based on sentiment analysis</v-list-item>
+      </v_list>
 In particular, we will look at the top 10 countries with highest review output and try to find their (semantically) most popular beer. As mentioned above, we have yet no reason to believe that this ranking is more "useful" than a rating-based ranking, we just want to study to what extent these two are different for now.
 
 
