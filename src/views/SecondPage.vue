ced93345e6900d86867813cfd46ba3c533aba38f
Joanne Affolter <joanne.affolter@epfl.ch>
2022-12-15 19:16:55 +0100

    Try1

--- src/views/SecondPage.vue
+++ src/views/SecondPage.vue
@@ -2,22 +2,53 @@
   <div>
   <v-card class="mb-6" color="primary">
     <v-card-title>
-      <p class="text-xl white--text font-weight-light">
+      <p class="text-h1 white--text font-weight-black">
         First insights from the beer reviews websites
       </p>
     </v-card-title>
   </v-card>
 
   <v-card>
-    We have chosen to base our market analysis on two beer review sites:
-    <span class= 'primary--text'> Beer Advocate </span> and
-    <span class= 'primary--text'> Rate Beer </span>.
-    &emsp These two sites are widely considered the go-to beer resource for
-    millions of consumers each month and the benchmark for beer reviews.
-    <br> Indeed, in order to understand the demand of our customers and to adapt
-    our offer as well as possible, <i>isn't it worth getting information directly from them ?</i>
-    <br>
+    <v-card-title></v-card-title>
+    <v-card-text>
+        We have chosen to base our market analysis on two beer review sites:
+        <span class= 'primary--text'> Beer Advocate </span> and
+        <span class= 'primary--text'> Rate Beer</span>.
+            These two sites are widely considered the go-to beer resource for
+        millions of consumers each month and the benchmark for beer reviews.
+        <br> Indeed, in order to understand the demand of our customers and to adapt
+        our offer as well as possible, <i>isn't it worth getting information directly from them ?</i>
+        <br>
+        We start our work with a first data exploratory of the two websites to understand the evolution
+        of the beer industry over the last years as well as the behavior of beer consumers.
+    </v-card-text>
   </v-card>
+
+  <v-card class="mb-6">
+    <v-card-title>
+      <p class="text-h2 primary--text font-weight-medium">
+        First insights from the beer reviews websites
+      </p>
+    </v-card-title>
+  </v-card>
+
+  <v-card>
+    <v-card-text>
+        We have chosen to base our market analysis on two beer review sites:
+        <span class= 'primary--text'> Beer Advocate </span> and
+        <span class= 'primary--text'> Rate Beer </span>.
+        &emsp These two sites are widely considered the go-to beer resource for
+        millions of consumers each month and the benchmark for beer reviews.
+        <br> Indeed, in order to understand the demand of our customers and to adapt
+        our offer as well as possible, <i>isn't it worth getting information directly from them ?</i>
+      <br>
+    </v-card-text>
+  </v-card>
+
+  <v-row>
+
+
+  </v-row>
 
 
   </div>
