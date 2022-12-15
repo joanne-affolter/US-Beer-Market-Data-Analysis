a6d749493fcf4ad6b22ebe6953d595fcae723d28
Joanne Affolter <joanne.affolter@epfl.ch>
2022-12-15 19:06:49 +0100

    Try1

--- src/views/SecondPage.vue
+++ src/views/SecondPage.vue
@@ -10,9 +10,9 @@
 
   <v-card>
     We have chosen to base our market analysis on two beer review sites:
-    <span color= 'primary'> Beer Advocate </span> and
-    <span color= 'primary'> Rate Beer </span>.
-    These two sites are widely considered the go-to beer resource for
+    <spann class= 'primary--text'> Beer Advocate </spann> and
+    <span class= 'primary--text'> Rate Beer </span>.
+    &emsp These two sites are widely considered the go-to beer resource for
     millions of consumers each month and the benchmark for beer reviews.
     <br> Indeed, in order to understand the demand of our customers and to adapt
     our offer as well as possible, <i>isn't it worth getting information directly from them ?</i>
