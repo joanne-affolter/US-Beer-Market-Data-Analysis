4bdcc466865295415158a5b66991a1f0615ecf2f
Joanne Affolter <joanne.affolter@epfl.ch>
2022-12-15 18:58:50 +0100

    Try1

--- src/views/SecondPage.vue
+++ src/views/SecondPage.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
   <v-card class="mb-6" color="primary">
-    <p class="text-xl white--text font-weight-light">
-      First insights from the beer reviews websites
-    </p>
+    <v-card-title>
+      <p class="text-xl white--text font-weight-light">
+        First insights from the beer reviews websites
+      </p>
+    </v-card-title>
   </v-card>
 
   <v-card>
@@ -12,9 +14,9 @@
     <span color= 'primary'> Rate Beer </span>.
     These two sites are widely considered the go-to beer resource for
     millions of consumers each month and the benchmark for beer reviews.
-    Indeed, in order to understand the demand of our customers and to adapt
-    our offer as well as possible, isn't it worth getting information directly from them ?
-
+    <br> Indeed, in order to understand the demand of our customers and to adapt
+    our offer as well as possible, <i>isn't it worth getting information directly from them ?</i>
+    <br>
   </v-card>
 
 
