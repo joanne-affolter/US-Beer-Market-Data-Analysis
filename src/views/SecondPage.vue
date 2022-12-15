dce83abcff9753579ad3675342c3d79ed2af18b5
Joanne Affolter <joanne.affolter@epfl.ch>
2022-12-15 19:38:23 +0100

    Try1

--- src/views/SecondPage.vue
+++ src/views/SecondPage.vue
@@ -3,13 +3,13 @@
   <v-card class="mb-6" color="primary">
     <v-card-title>
       <p class="text-h3 white--text font-weight-black">
-        First insights from the beer reviews websites
+        First insights from the beer reviews
+        <br>websites
       </p>
     </v-card-title>
   </v-card>
 
   <v-card class="mb-6">
-    <v-card-title></v-card-title>
     <v-card-text>
         We have chosen to base our market analysis on two beer review sites:
         <span class= 'primary--text'> Beer Advocate </span> and
@@ -26,7 +26,7 @@
 
   <v-card class="mb-6">
     <v-card-title>
-      <p class="text-h4 primary--text font-weight-medium">
+      <p class="text-h4 primary--text font-weight-black">
         From where the customers are coming from ?
       </p>
     </v-card-title>
@@ -34,24 +34,34 @@
 
   <v-card>
     <v-card-text>
-        We have chosen to base our market analysis on two beer review sites:
-        <span class= 'primary--text'> Beer Advocate </span> and
-        <span class= 'primary--text'> Rate Beer </span>.
-        &emsp These two sites are widely considered the go-to beer resource for
-        millions of consumers each month and the benchmark for beer reviews.
-        <br> Indeed, in order to understand the demand of our customers and to adapt
-        our offer as well as possible, <i>isn't it worth getting information directly from them ?</i>
-      <br>
+      <v-row>
+        <v-col cols="8" sm="8" lg="8" md="8">
+          <v-card>
+            <v-img
+
+              src="@/assets/images/nb_ratings_country.png"
+            ></v-img>
+          </v-card>
+        </v-col>
+        <v-col cols="8" sm="8" lg="8" md="8">
+          <v-card>
+            <v-card-text>
+              On the above figure we plotted the number of ratings posted in US
+              in blue and the number of ratings for all the other countries in yellow.
+              The y-axis is in logarithmic scale.
+              <br>We can see that the total number of rating from the US is much higher
+              than in the other countries. There are nearly 10 millions ratings from the US whereas the second country (Canada) only has 1 million.
+            </v-card-text>
+        </v-card>
+        </v-col>
+      </v-row>
     </v-card-text>
   </v-card>
 
   <v-row>
 
     <v-card>
-      <v-img
-          height="200"
-          src="@/assets/images/nb_ratings_country.png"
-        ></v-img>
+
     </v-card>
   </v-row>
 
