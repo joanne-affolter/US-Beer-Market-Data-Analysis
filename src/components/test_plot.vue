f92aa9cddebe7fa83619df095b00dc384ba4383f
Joanne Affolter <joanne.affolter@epfl.ch>
2022-12-16 15:05:47 +0100

    fff

---
+++ src/components/test_plot.vue
@@ -0,0 +1,97 @@
+
+
+
+<script>
+var data = [
+  {
+    type: 'choropleth',
+    name: 'US states',
+    locations: [
+      'AL',
+      'AK',
+      'AZ',
+      'AR',
+      'CA',
+      'CO',
+      'CT',
+      'DE',
+      'FL',
+      'GA',
+      'HI',
+      'ID',
+      'IL',
+      'IN',
+      'IA',
+      'KS',
+      'KY',
+      'LA',
+      'ME',
+      'MD',
+      'MA',
+      'MI',
+      'MN',
+      'MS',
+      'MO',
+      'MT',
+      'NE',
+      'NV',
+      'NH',
+      'NJ',
+      'NM',
+      'NY',
+      'NC',
+      'ND',
+      'OH',
+      'OK',
+      'OR',
+      'PA',
+      'RI',
+      'SC',
+      'SD',
+      'TN',
+      'TX',
+      'UT',
+      'VT',
+      'VA',
+      'WA',
+      'WV',
+      'WI',
+      'WY',
+    ],
+    locationmode: 'USA-states',
+    scope: 'usa',
+    color_continuous_scale: 'Reds',
+    z: [
+      141, 140, 155, 147, 132, 146, 151, 137, 146, 136, 145, 141, 149, 151, 138, 158, 164, 141, 146, 145, 142, 150, 155,
+      160, 156, 161, 147, 164, 150, 152, 155, 167, 145, 146, 151, 154, 161, 145, 155, 150, 151, 162, 172, 169, 170, 151,
+      152, 173, 160, 176,
+    ],
+    zmin: 25,
+    zmax: 280,
+  },
+]
+
+Plotly.newPlot('myDiv', data, layout, config)
+</script>
+
+
+
+<script>
+import Plotly from 'plotly.js-dist/plotly';
+
+export default {
+  name: "PlotlyTest",
+  data: function() {
+    return {
+      count: 0,
+      data:[{ x: [1,2,3,4], y: [10,15,13,17], type:"scatter" }],
+      attr: { displayModeBar: true },
+      layout: { title: "My graph :)" }
+    };
+  },
+  methods: {
+
+  },
+  mounted() {
+    Plotly.plot('plot1', this.data);
+  }
---
+++ src/views/ThirdPage.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <!--Title with image-->
+        <v-card class="mb-6" color="primary">
+            <v-img
+                height="200"
+                width="100%"
+                src="@/assets/images/materio_img/mac_pc.png"
+            ></v-img>
+
+            <v-card-text class="d-flex align-center mt-5 text-h5 white--text font-weight-black">
+                Market analysis
+            </v-card-text>
+        </v-card>
+
+        <!--Introduction-->
+        <v-card class="mb-6">
+        <v-card-text>
+            In this second part, we will determine who are the potential competitors of our client
+            and in which state our client should locate its brewery.
+        </v-card-text>
+        </v-card>
+
+        <v-card>
+            <v-card-title class="align-start">
+                Let's note we were given the following specifications :
+                <v-spacer></v-spacer>
+            </v-card-title>
+
+            <v-card-text>
+                <v-timeline dense class="timeline-custom-dense timeline-custom-dots">
+
+                <v-timeline-item small color="primary">
+                    <div class="d-flex justify-space-between align-center flex-wrap">
+                    <h4 class="font-weight-semibold me-1">
+                        Passionate about beer, he wants to be as close as possible to his customers and therefore wants to reach a local clientele in the state where he will set up his brewery.
+                    </h4>
+                    </div>
+                </v-timeline-item>
+
+                <v-timeline-item small color="primary">
+                    <div class="d-flex justify-space-between align-center flex-wrap">
+                    <h4 class="font-weight-semibold me-1">
+                        He is not able to produce more than 30 beers.
+                    </h4>
+                    </div>
+                </v-timeline-item>
+
+                <v-timeline-item small color="primary">
+                    <div class="d-flex justify-space-between align-center flex-wrap">
+                    <h4 class="font-weight-semibold me-1">
+                        He is open to all states to set up his brewery.
+                    </h4>
+                    </div>
+                </v-timeline-item>
+
+                </v-timeline>
+            </v-card-text>
+        </v-card>
+
+    </div>
+</template>
+
+<script>
+</script>
