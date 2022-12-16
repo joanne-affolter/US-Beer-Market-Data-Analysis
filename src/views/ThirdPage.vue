03e892d6ea3ee1d70327b334b4864fb77de14f62
Joanne Affolter <joanne.affolter@epfl.ch>
2022-12-16 15:46:41 +0100

    fff

--- src/views/ThirdPage.vue
+++ src/views/ThirdPage.vue
@@ -9,8 +9,10 @@
             ></v-img>
 
             <v-card-text class="d-flex align-center mt-5 text-h5 white--text font-weight-black">
+                <span class="text-h5">
                 Market analysis
-                <span class="text-h6">
+                </span>
+                <span class="text-subtitle-2">
                     In this second part, we will determine who are the potential competitors of our client
                     and in which state he should locate its brewery.
                 </span>
@@ -57,58 +59,89 @@
         </v-card>
 
         <!--I. Title with avatar-->
-        <v-card class="mb-6" color="primary">
-        <v-container>
-            <v-row>
+        <v-row>
+        <v-col cols="12" md="7" lg="7">
+          <v-card>
+            <v-row class="ma-0 pa-0">
+              <v-col
+                cols="8"
+                sm="6"
+              >
+                <v-card-title class="greeting-title d-flex flex-nowrap text-2xl">
+                    Where should you open your brewery ?
+                </v-card-title>
+              </v-col>
 
-            <v-col cols="8" sm="6">
-                <v-card-text class="d-flex align-center mt-5 text-h5 white--text font-weight-black">
-                    Where should you open you brewery ?
-                </v-card-text>
-            </v-col>
-
-            <v-col cols="4" sm="6">
-                <v-img
-                width="121"
-                src="@/assets/images/3d-characters/pose-2.png"
-                class="gamification-john-pose-2"
-                ></v-img>
-            </v-col>
-
+              <v-col
+                cols="4"
+                sm="6"
+              >
+                <div>
+                  <v-img
+                    width="120"
+                    src="@/assets/images/misc/tree-4.png"
+                    class="gamification-tree-4"
+                  ></v-img>
+                  <v-img
+                    width="121"
+                    src="@/assets/images/3d-characters/pose-2.png"
+                    class="gamification-john-pose-2"
+                  ></v-img>
+                  <v-img
+                    width="100"
+                    src="@/assets/images/misc/tree.png"
+                    class="gamification-tree"
+                  ></v-img>
+                </div>
+              </v-col>
             </v-row>
-        </v-container>
-        </v-card>
+          </v-card>
+        </v-col>
+    </v-row>
 
         <!-- Introduction -->
-            <!--Introduction-->
-            <v-card class="mb-6">
+        <v-card class="mb-6">
                 <v-card-text>
                     In order to determine which states our client should open its brewery in, we will chose the top 3 states based on thses criterias :
                 </v-card-text>
                 <v-actions>
-                    <v-row align='center' justify="center">
+                    <v-row >
 
                         <v-col>
-                            <v-btn elevation="13" rounded text x-large color="#F3E5F5">
+                            <v-btn elevation="13" rounded text x-large dark color="primary">
                                 A large market share to reach with a lot of potential clients
                             </v-btn>
                         </v-col>
 
+                    </v-row>
+                    <v-row align='center' justify="center">
                         <v-col>
-                            <v-btn elevation="13" rounded text x-large color="#E1BEE7">
+                            <v-btn elevation="13" rounded text x-large dark color="primary">
                                 Consumers who like local beers
                             </v-btn>
                         </v-col>
 
                         <v-col>
-                            <v-btn color='#CE93D8' elevation="13" rounded text x-large>
+                            <v-btn elevation="13" rounded text x-large dark color="primary">
                                 Beer lovers who make a lot of ratings
                             </v-btn>
                         </v-col>
-
                     </v-row>
                 </v-actions>
-            </v-card>
+        </v-card>
+
+        <!-- 1. States with a large market share to reach -->
+        <v-card class="mb-6" color="primary">
+        <v-container>
+            <v-row>
+            <v-col cols="8" sm="6">
+                <v-card-text class="d-flex align-center mt-5 text-h5 white--text font-weight-black">
+                    States with a large market share to reach
+               </v-card-text>
+            </v-col>
+            </v-row>
+        </v-container>
+        </v-card>
 
 
 
