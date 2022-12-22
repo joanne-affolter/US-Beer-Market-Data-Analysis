abb7a0f6f8c2205a21595f10b115aee720038ca1
Joanne Affolter <joanne.affolter@epfl.ch>
2022-12-22 22:45:52 +0100

    Thursday

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,121 +1,248 @@
 <template>
-  <div>
-    <!-- 1ere ligne - Welcome Anthony + Liste des chantiers en cours -->
-    <v-row>
-      <ConfettiExplosion :colors=[primary, warning, success, primary] />
-    </v-row>
-    <v-row>
-      <!-- Welcome Anthony ! -->
-      <v-col cols="12" md="7" lg="7">
-        <v-card>
-          <v-row class="ma-0 pa-0">
-            <v-col
-              cols="8"
-              sm="6"
-            >
-              <v-card-title class="greeting-title d-flex flex-nowrap text-2xl">
-                <span class="text-no-wrap">First Part</span>
-                <span class="text-no-wrap font-weight-bold mx-1">TEST</span>
-                <span>🎉</span>
-              </v-card-title>
+  <v-card
+    id="pricing-plan"
+    class="text-center py-sm-15 py-5"
+  >
+    <v-card-text>
+      <v-row>
+        <v-col
+          cols="12"
+          md="6"
+          sm="8"
+          class="mx-auto"
+        >
+          <h1 class="font-weight-medium mb-5">
+            teAmDreAm
+          </h1>
+          <p class="mb-3">
+            We are a consulting firm that specializes in helping breweries succeed through the use of data analytics. Our team has expertise in the brewery industry and utilizes a wide range of data sources to provide our clients with informed and strategic advice.
+          </p>
+        </v-col>
+      </v-row>
+    </v-card-text>
 
-              <v-card-text>
-                <span>Introduction </span>
-                <span class="text--primary font-weight-semibold">blabla</span>
-              </v-card-text>
-            </v-col>
+    <!-- pricing free trial -->
+    <v-card-text class="pricing-free-trial my-16">
+      <v-row>
+        <v-col
+          cols="12"
+          md="10"
+          class="mx-auto"
+        >
+          <div class="pricing-trial-content d-flex justify-space-between flex-column flex-md-row">
+            <div class="text-center text-md-left mt-7">
+              <p class="text-2xl font-weight-medium primary--text mb-2">
+                Our mission
+              </p>
+              <p class="text-base">
+                Our client, Sam Smith, recently graduated from college in Massachusetts and is interested in starting a brewery.
+              </p>
+              <p class="text-base">
+                He has noticed that local breweries are now trendy and wants to create a local vibe for his beer. He has already taken the first steps towards this goal by creating some sample beers and seeking feedback from his family and friends.
+              </p>
+              <p class="text-base">
+                However, he has received differing opinions on the quality and potential success of his brews, leaving him unsure of how to move forward. Seeking a more objective perspective, Sam has decided to turn to our consulting service for guidance.
+              </p>
+            </div>
 
-            <v-col
-              cols="4"
-              sm="6"
+            <!-- images -->
+            <v-img
+              contain
+              height="278"
+              max-width="250"
+              src="@/assets/images/3d-characters/pose-fs-9.png"
+              class="pricing-trial-img mx-auto"
+              alt="svg img"
+            />
+            <!--/ images -->
+          </div>
+        </v-col>
+      </v-row>
+    </v-card-text>
+    <!--/ pricing free trial -->
+
+    <v-card-text>
+      <h2 class="text-2xl font-weight-medium mb-2">
+        FAQ's
+      </h2>
+      <p>Let us help answer the most common questions.</p>
+    </v-card-text>
+
+    <v-card-text class="pricing-accordion mt-1">
+      <v-row>
+        <v-col
+          cols="12"
+          md="8"
+          class="mx-auto text-left"
+        >
+          <v-expansion-panels v-model="pricingAccordion">
+            <v-expansion-panel
+              v-for="item in faqsList"
+              :key="item.title"
             >
-              <div>
-                <v-img
-                  width="120"
-                  src="@/assets/images/misc/tree-4.png"
-                  class="gamification-tree-4"
-                ></v-img>
-                <v-img
-                  width="121"
-                  src="@/assets/images/3d-characters/pose-2.png"
-                  class="gamification-john-pose-2"
-                ></v-img>
-                <v-img
-                  width="100"
-                  src="@/assets/images/misc/tree.png"
-                  class="gamification-tree"
-                ></v-img>
-              </div>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-col>
-  </v-row>
-  </div>
+              <v-expansion-panel-header>
+                {{ item.title }}
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                {{ item.desc }}
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
-import ConfettiExplosion from 'vue-confetti-explosion'
+import { mdiCheckboxBlankCircleOutline } from '@mdi/js'
+import { ref } from '@vue/composition-api'
 
 export default {
-  data: () => ({}),
-  computed: {},
-  mounted() {},
-  methods: {},
+  setup() {
+    const status = ref(false)
+    const pricingAccordion = ref(0)
+
+    const pricing = {
+      basicPlan: {
+        title: 'Basic',
+        monthlyPrice: 0,
+        currentPlan: true,
+        popularPlan: false,
+        subtitle: 'A simple start for everyone',
+        imgSrc: require('@/assets/images/misc/pricing-tree-1.png'),
+        yearlyPlan: {
+          perMonth: 0,
+          totalAnnual: 0,
+        },
+        planBenefits: [
+          '100 responses a month',
+          'Unlimited forms and surveys',
+          'Unlimited fields',
+          'Basic form creation tools',
+          'Up to 2 subdomains',
+        ],
+      },
+      standardPlan: {
+        monthlyPrice: 49,
+        title: 'Standard',
+        popularPlan: true,
+        currentPlan: false,
+        subtitle: 'For small to medium businesses',
+        imgSrc: require('@/assets/images/misc/pricing-tree-2.png'),
+        yearlyPlan: {
+          perMonth: 40,
+          totalAnnual: 480,
+        },
+        planBenefits: [
+          'Unlimited responses',
+          'Unlimited forms and surveys',
+          'Instagram profile page',
+          'Google Docs integration',
+          'Custom “Thank you” page',
+        ],
+      },
+      enterprisePlan: {
+        monthlyPrice: 99,
+        popularPlan: false,
+        currentPlan: false,
+        title: 'Enterprise',
+        subtitle: 'Solution for big organizations',
+        imgSrc: require('@/assets/images/misc/pricing-tree-3.png'),
+        yearlyPlan: {
+          perMonth: 80,
+          totalAnnual: 960,
+        },
+        planBenefits: [
+          'PayPal payments',
+          'Logic Jumps',
+          'File upload with 5GB storage',
+          'Custom domain support',
+          'Stripe integration',
+        ],
+      },
+    }
+
+    const faqsList = [
+      {
+        title: 'General settings',
+        desc: 'Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar diam eros in elit. Pellentesque convallis laoreet laoreet.Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar diam eros in elit. Pellentesque convallis laoreet laoreet.',
+      },
+      {
+        title: 'Users',
+        desc: 'Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar diam eros in elit. Pellentesque convallis laoreet laoreet.Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar diam eros in elit. Pellentesque convallis laoreet laoreet.',
+      },
+      {
+        title: 'Personal data',
+        desc: 'Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar diam eros in elit. Pellentesque convallis laoreet laoreet.Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar diam eros in elit. Pellentesque convallis laoreet laoreet.',
+      },
+      {
+        title: 'Advanced settings',
+        desc: 'Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar diam eros in elit. Pellentesque convallis laoreet laoreet.Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar diam eros in elit. Pellentesque convallis laoreet laoreet.',
+      },
+    ]
+
+    return {
+      status,
+      pricing,
+      faqsList,
+      pricingAccordion,
+      icons: {
+        mdiCheckboxBlankCircleOutline,
+      },
+    }
+  },
 }
 </script>
 
-<style lang="scss" scoped>
-.gamification-tree-4,
-.gamification-john-pose-2,
-.gamification-tree {
-  position: absolute;
-}
-.gamification-tree {
-  top: 10%;
-  right: 0;
-}
-.gamification-john-pose-2 {
-  bottom: 0;
-  right: 15%;
-}
-.gamification-tree-4 {
-  bottom: 0;
-  right: 30%;
-}
-@media (max-width: 600px) {
-  .gamification-tree,
-  .gamification-tree-4 {
-    display: none;
+<style lang="scss" >
+@import '@core/preset/preset/mixins';
+@import '@core/preset/preset/variables';
+
+#pricing-plan {
+  .popular {
+    border-color: rgba(145, 85, 253, 0.3);
   }
-  .gamification-john-pose-2 {
-    right: 5%;
-  }
-}
-@media (max-width: 500px) {
-  .gamification-john-pose-2 {
-    max-width: 70px;
-  }
-}
-@media (max-width: 400px) {
-  .greeting-title {
-    font-size: 1.2rem !important;
-  }
-}
-// rtl
-.v-application {
-  &.v-application--is-rtl {
-    .gamification-john-pose-2 {
-      right: initial;
-      left: 15%;
+
+  .pricing-free-trial {
+    background-color: rgba(145, 85, 253, 0.04);
+    position: relative;
+    height: 14.625rem;
+    .pricing-trial-img {
+      position: relative;
+      top: -3.5rem;
+      right: 0;
     }
-    .gamification-tree {
-      right: initial;
-      left: 0;
+  }
+  .pricing-accordion {
+    .v-expansion-panels {
+      .v-expansion-panel {
+        box-shadow: none !important;
+        margin-bottom: -1px;
+        &::before {
+          box-shadow: none;
+        }
+      }
     }
-    .gamification-tree-4 {
-      left: 30%;
-      right: initial;
+  }
+}
+
+@media (max-width: 960px) {
+  #pricing-plan {
+    .pricing-free-trial {
+      height: auto;
+      .pricing-trial-img {
+        bottom: -1.2rem;
+        top: unset;
+      }
+    }
+  }
+}
+
+@include theme--child(pricing-accordion) using ($material) {
+  .v-expansion-panels {
+    .v-expansion-panel {
+      border: 1px solid map-deep-get($material, 'dividers');
     }
   }
 }
