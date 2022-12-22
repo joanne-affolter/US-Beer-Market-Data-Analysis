bd7df1d3fa41289f60779fcc4d8abcf88449dc0a
Joanne Affolter <joanne.affolter@epfl.ch>
2022-12-22 16:05:34 +0100

    Thursday

--- src/views/FifthPage.vue
+++ src/views/FifthPage.vue
@@ -10,7 +10,7 @@
 
             <v-card-text >
                     <p class = "d-flex align-center mt-5 text-h5 white--text font-weight-black">Strategic advice for market positioning in Virginia</p>
-                    <p class = 'd-flex align-center mt-5 text-subtitle-1 white--text font-weight-black'>The American beer industry has become increasingly competitive in recent years. It is therefore essential for our client to define a strategy in order to stand out from other breweries and to survive over the years.</p>
+                    <p class = 'd-flex align-center mt-5 text-subtitle-1 white--text'>The American beer industry has become increasingly competitive in recent years. It is therefore essential for our client to define a strategy in order to stand out from other breweries and to survive over the years.</p>
             </v-card-text>
         </v-card>
 
@@ -20,7 +20,7 @@
                 <v-row class="ma-0 pa-0">
                   <v-col cols="12" sm="12">
                     <v-card-text class="text-subtitle-2 font-weight-bold">
-                        <span class=" text-m my-6">
+                        <span class=" text-m my-6 primary--text">
                             In this section, we will study the market in Virginia in detail to give our client advice on how to position itself as a market leader.                        </span>
 
                         <v-timeline dense class="timeline-custom-dense timeline-custom-dots mt-6">
@@ -62,11 +62,14 @@
                 <v-container>
                 <v-row align="center">
                     <v-col cols="2" sm="2">
-                        <v-img src="@/assets/images/question_gif.gif">
+                        <v-img
+                        src="@/assets/images/question_gif.gif"
+                        height="90"
+                        width="90">
                         </v-img>
                     </v-col>
                     <v-col cols="10" sm="10">
-                        <span class="text-m my-6 primary--text">
+                        <span class="d-flex align-center mt-5 text-h6 primary--text font-weight-bold">
                             How to find similar beers ?
                         </span>
                     </v-col>
@@ -77,39 +80,54 @@
             <v-card-text>
                 <v-container>
                     <v-row>
-                        <v-col cols="6" sm="6">
+                        <v-col cols="5" sm="5">
                             <v-card class="mb-6">
                                 <v-card-text>
+                                    <div class = 'font-weight-bold'>
                                     1️⃣ Represent each beer by a vector with components :
-                                    <div class="font-weight-bold primary--text">
-                                    <br> ▪️ aroma
+                                    </div>
+                                    <div class="primary--text">
+                                    ▪️ aroma
                                     <br> ▪️ palate
                                     <br> ▪️ appearance
                                     <br> ▪️ taste
                                     </div>
-                                    <v-img src="@/assets/images/rep_beer.jpg">
-
+                                    <v-img
+                                    src="@/assets/images/rep_beer.jpg"
+                                    width="300">
                                     </v-img>
                                 </v-card-text>
                             </v-card>
                         </v-col>
-                        <v-col cols="6" sm="6">
-                            <v-row>
-                            <v-card class="mb-6" color="primary">
+                        <v-col cols="7" sm="7">
+                            <v-card>
                                 <v-card-text>
-                                    2️⃣ Use Euclidean distance to measure similarity between beers
+                                    Let's visualize our beer vectors. But since they live in a four-dimensional, we first need to apply a dimensionality reduction method. We have opted for PCA.
+                                    <v-img
+                                    src="@/assets/images/viz_beers.png">
+                                    </v-img>
+                                    We can see a linear dependency between the values of the two axes, with points located around the regression line with a stable variance. A sub-matching pattern seems to exist, meaning that the beers in the ratings have similarities even if we cannot interpret the values of the two axes as such.
                                 </v-card-text>
                             </v-card>
-                            </v-row>
-                            <v-row>
-                            <v-card class="mb-6" color="primary">
-                                <v-card-text>
-                                    3️⃣ Use Euclidean distance to measure similarity between beers
-                                </v-card-text>
-                            </v-card>
-                            </v-row>
                         </v-col>
                     </v-row>
+
+                    <v-row>
+                        <v-card class="mb-6" color="primary">
+                                <v-card-text class = 'font-weight-bold'>
+                                    2️⃣ Use <strong>Euclidean distance </strong> to measure similarity between beers
+                                </v-card-text>
+                            </v-card>
+                    </v-row>
+
+                    <v-row>
+                        <v-card class="mb-6" color="primary">
+                                <v-card-text class = 'font-weight-bold'>
+                                    3️⃣ Use <strong>k-Nearest Neighbors algorithm </strong> to get the neighbors of our client's beer which will be considered as the most similar ones
+                                </v-card-text>
+                            </v-card>
+                    </v-row>
+
                 </v-container>
             </v-card-text>
         </v-card>
