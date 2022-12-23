04a14b0eb2a46afae9b6b843892c0c46eda84645
toniolasebrai <56965445+toniolasebrai@users.noreply.github.com>
2022-12-23 18:55:47 +0100

    Add bold and space to Fourth Page

--- src/views/FourthPage.vue
+++ src/views/FourthPage.vue
@@ -20,14 +20,14 @@
               <v-col cols="9" sm="9">
                 <v-card-text class="text-subtitle-2 font-weight-bold">
                     <span class="font-weight-semibold text-m my-6 primary--text">
-                        Now that we have chosen 3 potential states where our client could set up his brewery, let's determine who are the potential competitors of our client in each of these states. To do this, we will use 3 criteria and find which breweries :
+                        Now that we have chosen 3 potential states where our client could set up his brewery, let's determine <strong>who are the potential competitors of our client in each of these states.</strong> To do this, we will use 3 criteria and find which breweries :
                     </span>
                     <v-timeline dense class="timeline-custom-dense timeline-custom-dots mt-6">
 
                         <v-timeline-item small color="primary">
                             <div class="d-flex justify-space-between align-center flex-wrap">
                             <h4 class="font-weight-semibold me-1">
-                                Are the most popular with the highest number of ratings
+                                Are the <strong>most popular</strong> with the <strong>highest number of ratings</strong>
                             </h4>
                             </div>
                         </v-timeline-item>
@@ -35,7 +35,7 @@
                         <v-timeline-item small color="primary">
                             <div class="d-flex justify-space-between align-center flex-wrap">
                             <h4 class="font-weight-semibold me-1">
-                                Have been established in the area for a long time with the longest period since the first rating
+                                Have been established in the area for a long time with the <strong>longest period since the first rating</strong>
                             </h4>
                             </div>
                         </v-timeline-item>
@@ -43,7 +43,7 @@
                         <v-timeline-item small color="primary">
                             <div class="d-flex justify-space-between align-center flex-wrap">
                             <h4 class="font-weight-semibold me-1">
-                                Are the local breweries with the highest percentage of ratings within the state
+                                Are the <strong>local breweries</strong> with the <strong>highest percentage of ratings</strong> within the state
                             </h4>
                             </div>
                         </v-timeline-item>
@@ -51,8 +51,8 @@
                         </v-timeline>
 
                         <div class="text-subtitle-2 font-weight-semibold my-6">
-                            By examining these different criteria, we will assign scores to each brewery and then determine which ones are the strongest on the market and therefore which breweries represent the biggest competitors of our client.
-                            <br><br>In parallel, we are going to compare the 3 states retained to finally retain one for the next part of our work.
+                            By examining these different criteria, we will <strong>assign scores to each brewery</strong> and then determine <strong>which ones are the strongest on the market</strong> and therefore <strong>which breweries represent the biggest competitors</strong> of our client.
+                            <br><br>In parallel, we are going to <strong>compare the 3 states retained</strong> to finally <strong>retain one</strong> for the next part of our work.
                         </div>
                     </v-card-text>
               </v-col>
@@ -99,7 +99,7 @@
                     <v-container><v-row align="center" justify="center">
                         <v-col cols="10" sm="10"><v-img src="@/assets/images/wash1.png"></v-img></v-col></v-row>
                     <v-row>
-                        The 3 breweries with the highest number of comments dominate by far the other breweries in the top 10, and are the breweries to watch. <strong>Yakima Brewing and Malting Co. </strong> is far ahead of its competitors with <strong> 1,627 ratings </strong>, twice as many as the brewery just behind it.
+                        The 3 breweries with the highest number of comments dominate by far the other breweries in the top 10, and are the breweries to watch. <strong>Yakima Brewing and Malting Co. </strong> is far ahead of its competitors with  <strong> 1,627 ratings </strong>, twice as many as the brewery just behind it.
                 </v-row></v-container>
             </v-card-text>
             </v-card>
@@ -111,7 +111,7 @@
                     <v-container><v-row align="center" justify="center">
                         <v-col cols="10" sm="10"><v-img src="@/assets/images/vir1.png"></v-img></v-col></v-row>
                     <v-row>
-                        The distribution of the number of ratings is more uniform among the breweries here. This is not surprising, as this is the state with the most breweries in the country (33 breweries) and therefore fewer market leaders like in Washington. Howewer, we can note that Blue & Gray Brewing Co. slightly dominates the market.
+                        The distribution of the <strong>number of ratings is more uniform</strong> among the breweries here. This is not surprising, as this is the state with the most breweries in the country (33 breweries) and therefore fewer market leaders like in Washington. Howewer, we can note that Blue & Gray Brewing Co. slightly dominates the market.
                     </v-row></v-container>
                 </v-card-text>
             </v-card>
@@ -123,7 +123,7 @@
                 <v-container><v-row align="center" justify="center">
                     <v-col cols="7"><v-img src="@/assets/images/m1.png"></v-img></v-col></v-row>
                 <v-row>
-                    The top 3 breweries here share more than half of the total number of ratings of the 10 most popular breweries, and are therefore the breweries to watch.
+                    The <strong>top 3 breweries</strong> here share <strong>more than half of the total number of ratings of the 10 most popular breweries</strong>, and are therefore the breweries to watch.
                 </v-row></v-container>
                 </v-card-text>
             </v-card>
@@ -133,7 +133,7 @@
 
     <v-card class="mb-6 my-6">
         <v-card-text class="text-subtitle-2">
-            Following the analysis of the number of ratings per brewery for each state, we can observe that there are generally fewer ratings per brewery in Virginia: the most popular brewery has only 346 ratings, while this number is much higher in Massachusetts (1627) and Virginia (1493). This means that there are fewer breweries dominating the market. The sector will be less competitive for our client, which is a positive point.
+            Following the analysis of the number of ratings per brewery for each state, we can observe that there are generally <strong>fewer ratings per brewery in Virginia:</strong> the <strong>most popular brewery</strong> has only <strong>346 ratings</strong>, while this number is <strong>much higher in Massachusetts (1627) and Virginia (1493)</strong>. This means that there are fewer breweries dominating the market. The sector will be <strong>less competitive</strong> for our client, which is a positive point.
             <br><br>
             Let's continue to fill the table containing the scores determined through our analysis. We recommend the state of Virginia for this criterion since it is the state with the least number of popular competitors (high number of ratings).
         </v-card-text>
@@ -154,7 +154,9 @@
 
     <v-card class="mb-6 my-8">
         <v-card-text class="font-weight-semibold">
-            In this analysis, we will identify breweries that are well-established in the market and have a reputation for quality among consumers. We will focus on breweries that have the most years of experience, as these are likely to have a strong presence in the market and be well-known by consumers. By identifying these breweries, we can better understand the competitive landscape and determine which ones may pose the greatest threat to our client's success.
+            In this analysis, we will identify breweries that are <strong>well-established in the market</strong> and <strong>have a reputation for quality among consumers</strong>. 
+            <br><br>
+            We will focus on breweries that have the most years of experience, as these are likely to have a strong presence in the market and be well-known by consumers. By identifying these breweries, we can better understand the competitive landscape and determine which ones may pose the greatest threat to our client's success.
         </v-card-text>
     </v-card>
 
@@ -172,9 +174,9 @@
                     <v-container><v-row align="center" justify="center">
                         <v-col cols="10" sm="10"><v-img src="@/assets/images/w2.png"></v-img></v-col></v-row>
                     <v-row>
-                        The oldest breweries in the comments have a significant amount of experience, with the first one having been in the area for 17 years and the second one for 16 years. The next three breweries have been in the area for 14 years. These breweries are well-established and may be considered institutions in Washington. 
+                        The oldest breweries in the comments have a significant amount of experience, with the first one having been in the area for 17 years and the second one for 16 years. The next three breweries have been in the area for 14 years. These breweries are well-established and <strong>may be considered institutions in Washington.</strong>
                         <br><br>
-                        Additionally, the large number of established breweries in the area may make it more difficult for new breweries to gain a foothold, as consumers may be more inclined to visit established breweries out of habit. As such, these breweries may pose a real threat to our client as potential competitors.
+                        Additionally, the large number of established breweries in the area may make it more difficult for new breweries to gain a foothold, as consumers may be more inclined to visit established breweries out of habit. As such, these breweries <strong>may pose a real threat to our client as potential competitors.</strong>
                     </v-row></v-container>
             </v-card-text>
             </v-card>
@@ -186,9 +188,9 @@
                     <v-container><v-row align="center" justify="center">
                         <v-col cols="8" sm="8"><v-img src="@/assets/images/v2.png"></v-img></v-col></v-row>
                     <v-row>
-                        In Virginia, there is a greater difference between long-established breweries and newer breweries than there is in Washington. The top three breweries in Virginia have been in the reviews for 16, 14, and 13 years, respectively, while the 10th brewery has only been rated for 3 years. This indicates that most of the breweries in Virginia are relatively new, with only 5 of the 33 breweries having been in the comments for more than 7 years. 
+                        In Virginia, there is <strong>a greater difference between long-established breweries and newer breweries</strong> than there is in Washington. The top three breweries in Virginia have been in the reviews for 16, 14, and 13 years, respectively, while the 10th brewery has only been rated for 3 years. This indicates that <strong>most of the breweries in Virginia are relatively new</strong>, with only 5 of the 33 breweries having been in the comments for more than 7 years. 
                         <br><br>
-                        Despite having the largest number of potential competitors among the states we analyzed, Virginia also has the highest score in terms of the evolution of the number of users over the years. This suggests that there may be more room for new breweries to succeed in Virginia compared to Washington, and that our client may be able to find a place in the market more easily in this state.    
+                        Despite having the largest number of potential competitors among the states we analyzed, Virginia also has <strong>the highest score in terms of the evolution of the number of users over the years</strong>. This suggests that <strong>there may be more room for new breweries to succeed in Virginia</strong> compared to Washington, and that our client may be able to find a place in the market more easily in this state.    
                     </v-row></v-container>
                 </v-card-text>
             </v-card>
@@ -202,7 +204,7 @@
                 <v-row>
                     In Massachusetts, 4 breweries are present in the comments since 15 years and dominate the other breweries in the top 10. Except for Gardner Ale House, the period since the first rating is twice as long as in the other breweries.
                     <br><br>
-                    We find the same pattern as in Virginia, with only 5 breweries appearing in the comments for more than 10 years. These are the ones to watch, but the market is quite favorable for our client.
+                    We find the same pattern as in Virginia, with only 5 breweries appearing in the comments for more than 10 years. <strong>These are the ones to watch, but the market is quite favorable for our client.</strong>
                 </v-row></v-container>
                 </v-card-text>
             </v-card>
@@ -245,7 +247,7 @@
                     <v-container><v-row align="center" justify="center">
                         <v-col cols="8" sm="8"><v-img src="@/assets/images/w3.png"></v-img></v-col></v-row>
                     <v-row>
-                        The percentage of local ratings is very high for these 10 breweries, ranging from 62% for Ghostfish Brewing Company to almost 95% for Top Rung Brewing Company. This suggests an audience in Washington with a preference for local beers, which is very interesting for our client.
+                        The percentage of local ratings is <strong>very high</strong> for these 10 breweries, ranging from 62% for Ghostfish Brewing Company to almost 95% for Top Rung Brewing Company. This suggests an audience in Washington with a preference for local beers, which is very interesting for our client.
                     </v-row>
                     </v-container>
                 </v-card-text>
@@ -258,8 +260,8 @@
                     <v-container><v-row align="center" justify="center">
                         <v-col cols="8" sm="8"><v-img src="@/assets/images/v3.png"></v-img></v-col></v-row>
                     <v-row>
-                        The percentage of local ratings is much lower in Virginia than in Washington. In fact, only 61% of the ratings are local for the first brewery, which is 1% less than the last brewery in the top 10 of Washington.
-                        <br><br>However, the population in Virginia seems to be more and more interested in local beers as it has the highest score for the evolution of the percentage of local ratings. Our client could therefore position itself as a market leader with its brewery.
+                        The percentage of local ratings is <strong>much lower</strong> in Virginia than in Washington. In fact, only 61% of the ratings are local for the first brewery, which is 1% less than the last brewery in the top 10 of Washington.
+                        <br><br>However, the population in Virginia <strong>seems to be more and more interested in local beers</strong> as it has the highest score for the evolution of the percentage of local ratings. Our client could therefore position itself as a market leader with its brewery.
                       </v-row></v-container>
                 </v-card-text>
             </v-card>
@@ -271,7 +273,7 @@
                 <v-container><v-row align="center" justify="center">
                     <v-col cols="8" sm="8"><v-img src="@/assets/images/m3.png"></v-img></v-col></v-row>
                 <v-row>
-                    Similarly to Virginia, the trend for local is less pronounced than in Washington. Moreover, the percentage of local ratings is quite tight for the breweries in the top 10. Down the Road Beer Company still stands out among the other breweries.
+                    Similarly to Virginia, <strong>the trend for local is less pronounced than in Washington.</strong> Moreover, the percentage of local ratings is quite tight for the breweries in the top 10. Down the Road Beer Company still stands out among the other breweries.
                 </v-row></v-container>
                 </v-card-text>
             </v-card>
